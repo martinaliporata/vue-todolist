c491de20535fa48cc1813d660596756f9211c783
martinaliporata <martinaliporata@gmail.com>
2024-05-31 07:20:05 +0200

    creato array di oggetti

--- js/script.js
+++ js/script.js
@@ -3,6 +3,36 @@ const {createApp} = Vue
 createApp ({
     data(){
         return {
+            toDoList: [
+                {
+                    name: 'patate',
+                    done: false,
+                },
+                {
+                    name: 'cipolle',
+                    done: false,
+                },
+                {
+                    name: 'zucchine',
+                    done: false,
+                },
+                {
+                    name: 'melanzane',
+                    done: false,
+                },
+                {
+                    name: 'vino',
+                    done: false,
+                },
+                {
+                    name: 'torta',
+                    done: false,
+                },
+                {
+                    name: 'latte',
+                    done: false,
+                },
+            ]
         }
     }
 }).mount('#app')
